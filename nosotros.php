0b6bbcfec24a92c870ce8c1d330d8995b2ea810b
ojedam13 <m_ojeda17@hotmail.com>
2022-03-24 17:53:57 -0300

    bibilioteca

---
+++ nosotros.php
@@ -0,0 +1,11 @@
+<?php include('template/cabecera.php')?>
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Hola somos un grupo de personas que se dedica al desarrolo web</p>
+</div>
+
+
+
+
+<?php include('template/pie.php') ?>
